6f62fade4b8cdfd54e129a34c84477dd4b1eec2e
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-07-04 09:38:05 +0530

    nested tag using react

--- Inception/App.js
+++ Inception/App.js
@@ -1,9 +1,30 @@
+/**
+ *
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>
+ *          </h1>
+ *      </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am inside h1 tag")
+  )
+);
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello world from React!"
 );
-
+console.log(heading); // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
