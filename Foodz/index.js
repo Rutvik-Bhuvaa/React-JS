2a63b17733727253a4f38ee5e400c298b5ff02a1
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-07-06 16:52:26 +0530

    Ignite the food delivery app project

---
+++ Foodz/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ * - logo
+ * - nav items
+ * Body
+ * - search bar
+ * - restaurant container
+ *      - restaurant card
+ * Footer
+ * - copyright
+ * - links
+ * - address
+ * - contact
+ */
